1c7bae3c2487f83440fdef3fa6d29349eb39b52d
christophhagen <christoph@spacemasters.eu>
2020-01-06 22:55:32 +0100

    Require most recent Ed25519 framework

--- Tests/Ed25519Tests/Ed25519Tests.swift
+++ Tests/Ed25519Tests/Ed25519Tests.swift
@@ -2,14 +2,20 @@ import XCTest
 @testable import Ed25519
 
 final class Ed25519Tests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        
+
+    func testSignature() throws {
+        let seed = try Seed()
+        let pair = KeyPair(seed: seed)
+
+        let a = [UInt8]("SomeString".data(using: .utf8)!)
+
+        let signature = pair.sign(a)
+
+        let result = try pair.publicKey.verify(signature: signature, message: a)
+        XCTAssertTrue(result)
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testSignature", testSignature),
     ]
 }
